4f1c79a3f9cced1df79916b5a52a6e766cdd0185
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-03-21 15:26:39 -0500

    Using provider to control the navigation bar

--- lib/pages/tabs_page.dart
+++ lib/pages/tabs_page.dart
@@ -1,17 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   const TabsPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        physics: const BouncingScrollPhysics(),
-        children: [
-          Container(color: Colors.red),
-          Container(color: Colors.green),
-        ],
+    return ChangeNotifierProvider(
+      create: (_) => _NavigationModel(),
+      child: Scaffold(
+        body: _Pages(),
+        bottomNavigationBar: _BottomNavigationBar(),
       ),
     );
+  }
+}
+
+class _BottomNavigationBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final navigationModel = Provider.of<_NavigationModel>(context);
+
+    return BottomNavigationBar(
+      currentIndex: navigationModel.actualPage,
+      onTap: (i) => navigationModel.actualPage = i,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: 'For You',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.public),
+          label: 'Headlines',
+        ),
+      ],
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      // physics: const BouncingScrollPhysics(),
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+      ],
+    );
+  }
+}
+
+//  Mixin ChangeNotifier: https://stackoverflow.com/questions/57243896/with-keyword-in-flutter
+class _NavigationModel with ChangeNotifier {
+  int _actualPage = 0;
+
+  int get actualPage => _actualPage;
+
+  set actualPage(int value) {
+    _actualPage = value;
+    notifyListeners();
   }
 }
